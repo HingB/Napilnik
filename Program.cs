d0dc943d148444926074a0dd49c6d908ed6cf70b
HingB <50633061+HingB@users.noreply.github.com>
2021-07-17 10:37:52 +0400

    Init weapon project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace IJShooter
+{
+    class Weapon
+    {
+        private int _damage;
+        private int _bulletsCount;
+
+        public void Fire(Player player)
+        {
+            player.TakeDamage(_damage);
+            _bulletsCount -= 1;
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+
+        public void TakeDamage(int damage)
+        {
+            if (damage < 0)
+                return;
+
+            _health -= damage;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.Fire(player);
+        }
+    }
+}
