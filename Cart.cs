e30945258b4136e4ba99a9f4e24ded9e21ef57a4
HingB <50633061+HingB@users.noreply.github.com>
2021-07-19 20:18:50 +0300

    Init shop

---
+++ Cart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store
+{
+    public class Cart
+    {
+        private Order _order;
+        private Shop _shop;
+        private List<Cell> _cells = new List<Cell>();
+
+        public Cart(Shop shop)
+        {
+            _shop = shop;
+        }
+
+        public Order Order()
+        {
+            int linkLength = 20;
+
+            PayLinkGenerator generator = new PayLinkGenerator();
+            _order = new Order(generator.GenerateString(linkLength));
+
+            return _order;
+        }
+
+        public void Add(Good newGood, int count)
+        {
+            Cell newCell = new Cell(newGood, count);
+            var good = _shop.Cells.FirstOrDefault(good => good.Good == newGood);
+
+            if (count > good.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+
+            int cellIndex = _cells.FindIndex(good => good.Good == newGood);
+
+            if (cellIndex == -1)
+                _cells.Add(newCell);
+            else
+                _cells[cellIndex].Merge(newCell);
+        }
+    }
+}
---
+++ PayLinkGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Store
+{
+    class PayLinkGenerator
+    {
+        private char[] _alphabet = { 'a', 'b', 'c', 'd', 'e',
+                             'f', 'g', 'h', 'i', 'j',
+                             'k', 'l', 'm', 'n', 'o',
+                             'p', 'q', 'r', 's', 't',
+                             'u', 'v', 'w', 'x', 'y', 'z' };
+
+        public string GenerateString(int linkLength)
+        {
+            Random random = new Random();
+            char[] genetatedChars = new char[linkLength];
+
+            for (int i = 0; i < linkLength; i++)
+            {
+                genetatedChars[i] = _alphabet[random.Next(0, _alphabet.Length)];
+            }
+
+            return new string(genetatedChars);
+        }
+    }
+}
